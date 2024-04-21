5b576c3eed046320445bde19bd7a05ad109869b0
ItsTropica <146852597+ItsTropica@users.noreply.github.com>
2024-04-22 00:09:27 +1000

    Update Rodents.java

--- Java Development/Rodents.java
+++ Java Development/Rodents.java
@@ -1,12 +1,3 @@
-/*
-Student ID: 22065462
-Name: Bora Durdag
-Campus: Sydney City
-Tutor Name: Christopher Stanton
-Class Day: Thursday
-Class Time: 10:30AM - 12:30PM
-*/
-
 import java.util.Scanner;
 
 public class Rodents {
